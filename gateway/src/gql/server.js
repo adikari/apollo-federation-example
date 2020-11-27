ac4af9748ea51f631e8bc67ef7385019c971745b
subash <me@subash.com.au>
2020-11-27 17:03:41 +1100

    implement gateway server

--- gateway/src/gql/server.js
+++ gateway/src/gql/server.js
@@ -1,27 +1,22 @@
 const { ApolloServer } = require('apollo-server-lambda');
-
 const { RemoteGraphQLDataSource, ApolloGateway } = require('@apollo/gateway');
+const { serviceList } = require('../serviceList');
 
 class AuthenticatedDataSource extends RemoteGraphQLDataSource {
-  willSendRequest({ request, context }) {
-    request.http.headers.set('x-api-key', context.apiKey);
+  constructor({ url, apiKey }) {
+    super({ url });
+    this.apiKey = apiKey;
+  }
+
+  willSendRequest({ request }) {
+    request.http.headers.set('x-api-key', this.apiKey);
   }
 }
 
 const gateway = new ApolloGateway({
-  serviceList: [
-    {
-      name: 'user',
-      url: 'http://localhost:3001/graphql',
-      apiKey: 'd41d8cd98f00b204e9800998ecf8427e'
-    },
-    {
-      name: 'course',
-      url: 'http://localhost:3002/graphql',
-      apiKey: 'd41d8cd98f00b204e9800998ecf8427e'
-    }
-  ],
-  buildService: ({ url }) => new AuthenticatedDataSource({ url })
+  serviceList,
+  buildService: ({ url, apiKey }) =>
+    new AuthenticatedDataSource({ url, apiKey })
 });
 
 const server = new ApolloServer({ gateway, subscriptions: false });
---
+++ gateway/src/serviceList.js
@@ -0,0 +1,14 @@
+const serviceList = [
+  {
+    name: 'user',
+    url: 'http://localhost:3001/graphql',
+    apiKey: 'd41d8cd98f00b204e9800998ecf8427e'
+  },
+  {
+    name: 'course',
+    url: 'http://localhost:3002/graphql',
+    apiKey: 'd41d8cd98f00b204e9800998ecf8427e'
+  }
+];
+
+module.exports = { serviceList };
